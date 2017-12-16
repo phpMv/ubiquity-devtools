f56a352e46908cb37d5981c8d0a3af77aa973b60
jcheron <myaddressmail@gmail.com>
2017-12-16 03:42:27 +0100

    Autoloader removed + composer file creation updated

--- project-files/index.php
+++ project-files/index.php
@@ -1,6 +1,5 @@
 <?php
 use Ubiquity\controllers\Startup;
-use Ubiquity\controllers\Autoloader;
 error_reporting(E_ALL);
 
 define('DS', DIRECTORY_SEPARATOR);
@@ -8,9 +7,7 @@ define('ROOT', realpath('app').DS);
 
 $config=include_once ROOT.'config/config.php';
 
-require_once ROOT.'Ubiquity/controllers/Autoloader.php';
 require_once ROOT.'./../vendor/autoload.php';
 
-Autoloader::register($config);
 require_once ROOT.'config/services.php';
 Startup::run($config,$_GET["c"]);
