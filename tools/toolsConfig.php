8a7d8648abdc478260cfc7c2395f0f2664117edd
jcheron <myaddressmail@gmail.com>
2018-04-02 03:54:24 +0200

    add diff2html css

--- tools/toolsConfig.php
+++ tools/toolsConfig.php
@@ -1,7 +1,8 @@
 <?php
 return [ "cdn" => [ "jquery" => "https://code.jquery.com/jquery-3.3.1.min.js",
 		"bootstrap" => [ "css" => "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css","js" => "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ],
-		"semantic" => [ "css" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.css","js" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.js" ] ],
+		"semantic" => [ "css" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.css","js" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.js" ],
+		"diff2html"=>["css"=>"https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.3.3/diff2html.min.css"]],
 		"composer" => [ "require" =>
 				[ "twig/twig" => "~1.0","mindplay/annotations" => "dev-master","phpmv/ubiquity" => "2.0.x-dev" ],
 				"autoload"=>["psr-4"=>[""=>"app/"]]
