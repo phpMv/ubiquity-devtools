fdb54588fbecacb01d575847164d685db24feba4
jcheron <myaddressmail@gmail.com>
2018-04-02 02:53:14 +0200

    Add git-php in composer file

--- tools/toolsConfig.php
+++ tools/toolsConfig.php
@@ -4,6 +4,7 @@ return [ "cdn" => [ "jquery" => "https://code.jquery.com/jquery-3.3.1.min.js",
 		"semantic" => [ "css" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.css","js" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.js" ] ],
 		"composer" => [ "require" =>
 				[ "twig/twig" => "~1.0","mindplay/annotations" => "dev-master","phpmv/ubiquity" => "2.0.x-dev" ],
-				"autoload"=>["psr-4"=>[""=>"app/"]]
+				"autoload"=>["psr-4"=>[""=>"app/"]],
+				"require-dev"=>["czproject/git-php"=>"^3.13"]
 		]
 ];
