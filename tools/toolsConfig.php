9df253dedcb97843b96fccb446d180ff635b0dbf
jcheron <myaddressmail@gmail.com>
2018-11-19 03:26:00 +0100

    Fix semantic ui state issue see https://github.com/Semantic-Org/Semantic-UI/issues/6323

--- tools/toolsConfig.php
+++ tools/toolsConfig.php
@@ -1,7 +1,7 @@
 <?php
 return [ "cdn" => [ "jquery" => "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js",
 		"bootstrap" => [ "css" => "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css","js" => "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ],
-		"semantic" => [ "css" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.css","js" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.js" ],
+		"semantic" => [ "css" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css","js" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js" ,"state" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/components/state.min.js" ],
 		"diff2html"=>["css"=>"https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.3.3/diff2html.min.css"]],
 		"composer" => [ "require" =>
 				[ "twig/twig" => "~1.0","mindplay/annotations" => "dev-master","phpmv/ubiquity" => "2.0.x-dev" ],
