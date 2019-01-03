7fd461e7c8b13ce490631c81b3c770465ca688e2
jcheron <myaddressmail@gmail.com>
2019-01-03 16:53:57 +0100

    Update toolsConfig for composer versions

--- tools/toolsConfig.php
+++ tools/toolsConfig.php
@@ -4,7 +4,8 @@ return [ "cdn" => [ "jquery" => "https://cdnjs.cloudflare.com/ajax/libs/jquery/3
 		"semantic" => [ "css" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css","js" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js" ,"state" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/components/state.min.js" ],
 		"diff2html"=>["css"=>"https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.3.3/diff2html.min.css"]],
 		"composer" => [ "require" =>
-				[ "twig/twig" => "~1.0","mindplay/annotations" => "dev-master","phpmv/ubiquity" => "2.0.x-dev" ],
+				[ "php"=>"^7.1","twig/twig" => "^2.0","mindplay/annotations" => "^1.3","phpmv/ubiquity" => "2.0.*" ],
+				"require-dev"=>["monolog/monolog" => "^1.24"],
 				"autoload"=>["psr-4"=>[""=>"app/"]]
 		]
 ];
