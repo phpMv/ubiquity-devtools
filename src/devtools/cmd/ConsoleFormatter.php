9beebfcec122173cde7847c000e5e03d534b1a7d
jcheron <myaddressmail@gmail.com>
2019-02-27 04:46:13 +0100

    Test console support colors

--- src/devtools/cmd/ConsoleFormatter.php
+++ src/devtools/cmd/ConsoleFormatter.php
@@ -4,7 +4,8 @@ namespace  Ubiquity\devtools\cmd;
 class ConsoleFormatter {
 	const BLACK='0;30',DARK_GREY='1;30',BLUE='0;34',LIGHT_BLUE='1;34',GREEN='0;32',LIGHT_GREEN='1;32',CYAN='0;36',LIGHT_CYAN='1;36',RED='0;31',LIGHT_RED='1;31',PURPLE='0;35',LIGHT_PURPLE='1;35',BROWN='0;33',YELLOW='1;33',LIGHT_GRAY='0;37',WIGHT='1;37';
 	const BG_BLACK='40',BG_RED='41',BG_GREEN='42',BG_YELLOW='43',BG_BLUE='44',BG_MAGENTA='45',BG_CYAN='46',BG_LIGHT_GRAY='47';
-	const BOLD='1',END='21';
+	const BOLD='1',END_BOLD='22',CLEAR='0';
+
 	/**
 	 * Returns a colored string
 	 * @param string $string
@@ -13,14 +14,17 @@ class ConsoleFormatter {
 	 * @return string
 	 */
 	public static function colorize($string, $color = null, $bgColor = null) {
+		if(!self::isSupported()){
+			return $string;
+		}
 		$coloredString = "";
 		if (isset($color)) {
-			$coloredString .= "\033[" . $color . "m";
+			$coloredString .= self::escape($color);
 		}
 		if (isset($bgColor)) {
-			$coloredString .= "\033[" . $bgColor . "m";
+			$coloredString .= self::escape($bgColor);
 		}
-		$coloredString .=  $string . "\033[0m";
+		$coloredString .=  self::escape(self::CLEAR);
 		return $coloredString;
 	}
 
@@ -30,8 +34,29 @@ class ConsoleFormatter {
 		return implode("\n", $lines);
 	}
 
-	public static function showInfo($content){
-		return self::colorize(self::formatContent($content),self::CYAN);
+	private static function escape($value){
+		return "\033[{$value}m";
+	}
+
+	public static function showInfo($content,$dColor=self::CYAN){
+		return self::colorize(self::formatContent($content),$dColor);
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public static function isSupported()
+	{
+		if (DIRECTORY_SEPARATOR === '\\') {
+			if (function_exists('sapi_windows_vt100_support') && @sapi_windows_vt100_support(STDOUT)) {
+				return true;
+			} elseif (getenv('ANSICON') !== false || getenv('ConEmuANSI') === 'ON') {
+				return true;
+			}
+			return false;
+		} else {
+			return function_exists('posix_isatty') && @posix_isatty(STDOUT);
+		}
 	}
 
 	public static function formatContent($content,$prefix='    · '){
@@ -65,8 +90,11 @@ class ConsoleFormatter {
 	}
 
 	public static function formatHtml($str){
+		if(!self::isSupported()){
+			return $str;
+		}
 		$reg='@<(b)>(.+?)</\1>@i';
-		return preg_replace($reg, "\033[".self::BOLD.'m$2'."\033[".self::END."m", $str);
+		return preg_replace($reg, self::escape(self::BOLD).'$2'.self::escape(self::END_BOLD), $str);
 	}
 }
 
