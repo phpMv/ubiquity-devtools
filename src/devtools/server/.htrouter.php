d1174e8f08d46f6ed7048831325c38617426786c
jcheron <myaddressmail@gmail.com>
2019-02-27 04:45:06 +0100

    Fix Admin link pb (https://github.com/phpMv/ubiquity/issues/8)

--- src/devtools/server/.htrouter.php
+++ src/devtools/server/.htrouter.php
@@ -1,6 +1,6 @@
 <?php
 $uri = ltrim(urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)),'/');
-if (!file_exists(__DIR__ . '/../' .$uri)) {
+if ($uri==null || !file_exists(__DIR__ . '/../' .$uri)) {
 	$_GET['c'] = $uri;
 	include_once '_index.php';
 	return true;
