039e40e346a349262d08f89842de43fd6610dab0
jcheron <myaddressmail@gmail.com>
2019-03-24 03:34:34 +0100

    [update] refactoring with vHeader & vFooter files

--- src/devtools/project-files/app/controllers/IndexController.php
+++ src/devtools/project-files/app/controllers/IndexController.php
@@ -1,16 +1,30 @@
 <?php
 namespace controllers;
 use Ubiquity\core\postinstall\Display;
+use Ubiquity\themes\ThemesManager;
 
- /**
+/**
  * Controller IndexController
  **/
 class IndexController extends ControllerBase{
 
 	public function index(){
+		$defaultPage=Display::getDefaultPage();
 		$links=Display::getLinks();
 		$infos=Display::getPageInfos();
-		$this->loadView("index.html",compact('links','infos'));
+
+		$activeTheme=ThemesManager::getActiveTheme();
+		if($activeTheme!=null){
+			$themes=Display::getThemes();
+			$this->loadView('@activeTheme/main/vMenu.html',compact('themes','activeTheme'));
+		}
+		$this->loadView($defaultPage,compact('defaultPage','links','infos','activeTheme'));
+	}
+
+
+	public function ct($theme){
+		$config=ThemesManager::saveActiveTheme($theme);
+		header("Location: ".$config['siteUrl']);
 	}
 
 }
