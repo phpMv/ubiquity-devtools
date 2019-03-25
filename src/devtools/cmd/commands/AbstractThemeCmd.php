e8b431876edf303e72123ed508f1d2e2196341dd
jcheron <myaddressmail@gmail.com>
2019-03-25 13:12:10 +0100

    Update AbstractThemeCmd.php

--- src/devtools/cmd/commands/AbstractThemeCmd.php
+++ src/devtools/cmd/commands/AbstractThemeCmd.php
@@ -4,14 +4,13 @@ namespace Ubiquity\devtools\cmd\commands;
 
 
 use Ubiquity\devtools\cmd\Console;
-use Ubiquity\themes\ThemesManager;
 
 abstract class AbstractThemeCmd extends AbstractCmd{
 
 	protected static function saveActiveTheme($what){
 		$answer=Console::question(sprintf("Would-you like to define %s as the active theme ?",$what),['y','n']);
 		if(Console::isYes($answer)){
-			ThemesManager::saveActiveTheme($what);
+			\Ubiquity\themes\ThemesManager::saveActiveTheme($what);
 		}
 	}
 }
