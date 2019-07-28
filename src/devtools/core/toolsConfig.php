a7e8c85015caa0d0a2f2e1e2aafe49b7dfa19d59
jcheron <myaddressmail@gmail.com>
2019-07-29 00:21:25 +0200

    Update toolsConfig.php

--- src/devtools/core/toolsConfig.php
+++ src/devtools/core/toolsConfig.php
@@ -4,7 +4,7 @@ return [ "cdn" => [ "jquery" => "https://cdnjs.cloudflare.com/ajax/libs/jquery/3
 		"semantic" => [ "css" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css","js" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js" ,"state" => "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/components/state.min.js" ],
 		"diff2html"=>["css"=>"https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.3.3/diff2html.min.css"]],
 		"composer" => [ "require" =>
-				[ "php"=>"^7.1","twig/twig" => "^2.0","mindplay/annotations" => "^1.3","phpmv/ubiquity" => "dev-multi-db" ],
+				[ "php"=>"^7.1","twig/twig" => "^2.0","phpmv/ubiquity" => "dev-multi-db" ],
 				"require-dev"=>["monolog/monolog" => "^1.24"],
 				"autoload"=>["psr-4"=>[""=>"app/"]
 				]
