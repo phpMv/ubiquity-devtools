488f18374199c3746a848791a3d7e48bcd3e9415
jcheron <myaddressmail@gmail.com>
2019-11-15 13:09:44 +0100

    update fomantic to 2.8.1

--- src/devtools/core/toolsConfig.php
+++ src/devtools/core/toolsConfig.php
@@ -1,7 +1,7 @@
 <?php
 return [ "cdn" => [ "jquery" => "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js",
 		"bootstrap" => [ "css" => "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css","js" => "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ],
-		"semantic" => [ "css" => "https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.7.8/semantic.min.css","js" => "https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.7.8/semantic.min.js" ],
+		"semantic" => [ "css" => "'https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.1/dist/semantic.min.css","js" => "'https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.1/dist/semantic.min.js" ],
 		"diff2html"=>["css"=>"https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.12.1/diff2html.min.css"]],
 		"composer" => [ "require" =>
 				[ "php"=>"^7.2","twig/twig" => "^2.0","phpmv/ubiquity" => "^2.3" ],
