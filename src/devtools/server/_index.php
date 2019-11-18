d14110b0a280094e275c441e8495ae86143e627d
jcheron <myaddressmail@gmail.com>
2019-11-18 20:25:08 +0100

    default debug=>true with php embedded server

--- src/devtools/server/_index.php
+++ src/devtools/server/_index.php
@@ -8,9 +8,9 @@ $config = include ROOT . 'config/config.php';
 $sConfig= include __DIR__.\DS.'config.php';
 $config ["siteUrl"] = 'http://'.$sConfig['host'].':'.$sConfig['port'].'/';
 $config ['sessionName'] = $sConfig['sessionName'];
+$config['debug']=true;
 if(class_exists("\\Monolog\\Logger")){
 	$config['logger']=function () use($sConfig){return new \Ubiquity\log\libraries\UMonolog($sConfig['sessionName'],\Monolog\Logger::INFO);};
-	$config['debug']=true;
 	Ubiquity\log\Logger::init($config);
 }
 require ROOT . './../vendor/autoload.php';
