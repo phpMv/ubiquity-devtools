fc118184c513604994141b30132b6234740e038f
jcheron <myaddressmail@gmail.com>
2019-11-19 17:53:43 +0100

    Fix logger pb with php embedded server

--- src/devtools/server/_index.php
+++ src/devtools/server/_index.php
@@ -8,11 +8,11 @@ $config = include ROOT . 'config/config.php';
 $sConfig= include __DIR__.\DS.'config.php';
 $config ["siteUrl"] = 'http://'.$sConfig['host'].':'.$sConfig['port'].'/';
 $config ['sessionName'] = $sConfig['sessionName'];
+require ROOT . './../vendor/autoload.php';
 $config['debug']=true;
 if(class_exists("\\Monolog\\Logger")){
 	$config['logger']=function () use($sConfig){return new \Ubiquity\log\libraries\UMonolog($sConfig['sessionName'],\Monolog\Logger::INFO);};
-	Ubiquity\log\Logger::init($config);
+	\Ubiquity\log\Logger::init($config);
 }
-require ROOT . './../vendor/autoload.php';
 require ROOT . 'config/services.php';
 \Ubiquity\controllers\Startup::run ( $config );
