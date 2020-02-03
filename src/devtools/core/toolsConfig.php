764ee7b4525d1db2df48a4aea54f10a9b4762664
jcheron <myaddressmail@gmail.com>
2020-02-03 23:41:11 +0100

    update to Monolog 2.0

--- src/devtools/core/toolsConfig.php
+++ src/devtools/core/toolsConfig.php
@@ -4,8 +4,8 @@ return [ "cdn" => [ "jquery" => "https://cdnjs.cloudflare.com/ajax/libs/jquery/3
 		"semantic" => [ "css" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.3/dist/semantic.min.css","js" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.3/dist/semantic.min.js" ],
 		"diff2html"=>["css"=>"https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.12.1/diff2html.min.css"]],
 		"composer" => [ "require" =>
-				[ "php"=>"^7.4","twig/twig" => "^2.0","phpmv/ubiquity" => "^2.3" ],
-			"require-dev"=>["monolog/monolog" => "^1.24","mindplay/annotations" => "^1.3","phpmv/ubiquity-dev"=>"^0.0"],
+				[ "php"=>"^7.4","twig/twig" => "^2.0","phpmv/ubiquity" => "^2.3","phpmv/ubiquity-mailer"=>"^0.0" ],
+			"require-dev"=>["monolog/monolog" => "^2.0","mindplay/annotations" => "^1.3","phpmv/ubiquity-dev"=>"^0.0"],
 				"autoload"=>["psr-4"=>[""=>"app/"]
 				]
 		]
--- src/devtools/project-files/index.php
+++ src/devtools/project-files/index.php
@@ -1,7 +1,7 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__.DS.'app'.DS);
-$config=include_once ROOT.'config/config.php';
-require_once ROOT.'./../vendor/autoload.php';
-require_once ROOT.'config/services.php';
+$config=include ROOT.'config/config.php';
+require ROOT.'./../vendor/autoload.php';
+require ROOT.'config/services.php';
 \Ubiquity\controllers\Startup::run($config);
