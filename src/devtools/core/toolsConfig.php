51b0ff60e0f1c461830a2d7c0e32c928e464f0ed
jcheron <myaddressmail@gmail.com>
2020-05-06 02:18:36 +0200

    Update semantic(2.4.8) & jquery(3.5.1) versions

--- src/devtools/core/toolsConfig.php
+++ src/devtools/core/toolsConfig.php
@@ -1,8 +1,8 @@
 <?php
-return [ "cdn" => [ "jquery" => "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js",
+return [ "cdn" => [ "jquery" => "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js",
 		"bootstrap" => [ "css" => "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css","js" => "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ],
-		"semantic" => [ "css" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.3/dist/semantic.min.css","js" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.3/dist/semantic.min.js" ],
-		"diff2html"=>["css"=>"https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.12.1/diff2html.min.css"]],
+		"semantic" => [ "css" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.4/dist/semantic.min.css","js" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.4/dist/semantic.min.js" ],
+		"diff2html"=>["css"=>"https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.12.2/diff2html.min.css"]],
 		"composer" => [ "require" =>
 				[ "php"=>"^7.4","twig/twig" => "^3.0","phpmv/ubiquity" => "^2.3","phpmv/ubiquity-mailer"=>"^0.0" ],
 			"require-dev"=>["monolog/monolog" => "^2.0","mindplay/annotations" => "^1.3","phpmv/ubiquity-dev"=>"^0.0"],
