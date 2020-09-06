a7db52d86a2353138233ca94ec1c2c94e34340a1
jcheron <myaddressmail@gmail.com>
2020-09-07 00:50:56 +0200

    add db offset param to info:models command

--- src/devtools/cmd/commands/InfoModelsCmd.php
+++ src/devtools/cmd/commands/InfoModelsCmd.php
@@ -1,22 +1,22 @@
 <?php
-
 namespace Ubiquity\devtools\cmd\commands;
 
 use Ubiquity\cache\CacheManager;
 use Ubiquity\devtools\utils\FrameworkParts;
 use Ubiquity\devtools\cmd\ConsoleFormatter;
 
-class InfoModelsCmd extends AbstractCmdModel{
+class InfoModelsCmd extends AbstractCmdModel {
 
-	public static function run(&$config,$options,$what){
-		$fields=self::getOption($options, 'f', 'fields','');
-		$selectedModels=self::getSelectedModels(self::getOption($options, 'm', 'models',null),$config);
+	public static function run(&$config, $options, $what) {
+		$fields = self::getOption($options, 'f', 'fields', '');
+		$dbOffset = self::getOption($options, 'd', 'database', 'default');
+		$selectedModels = self::getSelectedModels(self::getOption($options, 'm', 'models', null), $config);
 		CacheManager::start($config);
-		$models=CacheManager::getModels($config,true);
-		foreach ($models as $model){
-			if($selectedModels==null || array_search($model, $selectedModels)!==false){
-				$infos=FrameworkParts::getModelInfos($model,$config);
-				echo ConsoleFormatter::showInfo("Infos for <b>".$model."</b>\n");
+		$models = CacheManager::getModels($config, true, $dbOffset);
+		foreach ($models as $model) {
+			if ($selectedModels == null || \array_search($model, $selectedModels) !== false) {
+				$infos = FrameworkParts::getModelInfos($model, $config);
+				echo ConsoleFormatter::showInfo("Infos for <b>" . $model . "</b>\n");
 				self::displayModelInfo($fields, $infos, null, false);
 			}
 		}
