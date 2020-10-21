df8c81b5a08e39d2323452cd33015e3447c717c6
jcheron <myaddressmail@gmail.com>
2020-10-21 18:53:17 +0200

    Create _ngx.php

---
+++ src/devtools/server/_ngx.php
@@ -0,0 +1,18 @@
+<?php
+// ngx.php
+if (! defined('DS')) {
+	define('DS', DIRECTORY_SEPARATOR);
+	define('ROOT', __DIR__ . \DS . '..' . \DS . 'app' . \DS);
+}
+$config = include ROOT . 'config/config.php';
+$sConfig = include __DIR__ . \DS . 'ngx-config.php';
+$config["sessionName"] = $sConfig["sessionName"];
+$address = $sConfig['host'] . ':' . $sConfig['port'];
+$config["siteUrl"] = 'http://' . $address;
+require ROOT . './../vendor/autoload.php';
+\Ubiquity\servers\ngx\NgxServer::init($config);
+require ROOT . 'config/services.php';
+
+function handle() {
+	\Ubiquity\servers\ngx\NgxServer::handleRequest();
+}
