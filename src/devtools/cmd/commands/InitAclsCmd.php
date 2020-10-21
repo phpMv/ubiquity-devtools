96df44d59e4891972c82ed4293c779bfb414ee79
jcheron <myaddressmail@gmail.com>
2020-10-22 01:35:47 +0200

    Update InitAclsCmd.php

--- src/devtools/cmd/commands/InitAclsCmd.php
+++ src/devtools/cmd/commands/InitAclsCmd.php
@@ -3,6 +3,7 @@ namespace Ubiquity\devtools\cmd\commands;
 
 use Ubiquity\devtools\cmd\Command;
 use Ubiquity\devtools\cmd\ConsoleFormatter;
+use Ubiquity\devtools\cmd\Console;
 use Ubiquity\security\acl\AclManager;
 use Ubiquity\security\acl\persistence\AclCacheProvider;
 
@@ -17,6 +18,18 @@ use Ubiquity\security\acl\persistence\AclCacheProvider;
 class InitAclsCmd extends AbstractCmd {
 
 	public static function run(&$config) {
+		if (! \class_exists(\Ubiquity\acl\AclManager::class, true)) {
+			$answer = Console::question("Ubiquity-acl is not available. Would you like to install it now with composer?", [
+				"y",
+				"n"
+			]);
+			if (Console::isYes($answer)) {
+				\system('composer require phpmv/ubiquity-acl');
+			} else {
+				echo ConsoleFormatter::showMessage('aborted operation!', 'error', 'init-acls');
+				die();
+			}
+		}
 		AclManager::start();
 		AclManager::initFromProviders([
 			new AclCacheProvider()
