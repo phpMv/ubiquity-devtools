62beae94295d1065d2a3d5d4f4c8f1eef90717d7
jcheron <myaddressmail@gmail.com>
2020-12-26 18:42:42 +0100

    update for annotations or attributes

--- src/devtools/core/toolsConfig.php
+++ src/devtools/core/toolsConfig.php
@@ -7,8 +7,8 @@ return [
 			"js" => "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"
 		],
 		"semantic" => [
-			"css" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.6/dist/semantic.min.css",
-			"js" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.6/dist/semantic.min.js"
+			"css" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.7/dist/semantic.min.css",
+			"js" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.7/dist/semantic.min.js"
 		],
 		"diff2html" => [
 			"css" => "https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.12.2/diff2html.min.css"
@@ -18,12 +18,10 @@ return [
 		"require" => [
 			"php" => ">=7.4",
 			"twig/twig" => "^3.0",
-			"phpmv/ubiquity" => "^2.3",
-			"phpmv/ubiquity-mailer" => "^0.0"
+			"phpmv/ubiquity" => "^2.3"
 		],
 		"require-dev" => [
-			"monolog/monolog" => "^2.0",
-			"mindplay/annotations" => "^1.3",
+			"monolog/monolog" => "^2.2",
 			"phpmv/ubiquity-dev" => "^0.0"
 		],
 		"autoload" => [
