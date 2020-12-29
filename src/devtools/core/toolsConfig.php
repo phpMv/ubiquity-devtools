6bc8b83490d01cdab9176e36bbe0733be185a6b4
jcheron <myaddressmail@gmail.com>
2020-12-29 18:38:11 +0100

    Update toolsConfig.php

--- src/devtools/core/toolsConfig.php
+++ src/devtools/core/toolsConfig.php
@@ -22,7 +22,7 @@ return [
 		],
 		"require-dev" => [
 			"monolog/monolog" => "^2.2",
-			"phpmv/ubiquity-dev" => "^0.0"
+			"phpmv/ubiquity-dev" => ">=0.0"
 		],
 		"autoload" => [
 			"psr-4" => [
