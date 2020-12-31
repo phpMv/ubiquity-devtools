306aa7a33273dd362a4b5dfb6d07671e068941b4
jcheron <myaddressmail@gmail.com>
2020-12-31 15:43:29 +0100

    Fix new action pb
    Call to a member function asAnnotation() on null

--- src/devtools/cmd/commands/NewActionCmd.php
+++ src/devtools/cmd/commands/NewActionCmd.php
@@ -1,34 +1,38 @@
 <?php
-
 namespace Ubiquity\devtools\cmd\commands;
 
 use Ubiquity\devtools\core\ConsoleScaffoldController;
 use Ubiquity\devtools\cmd\ConsoleFormatter;
+use Ubiquity\cache\CacheManager;
 
-class NewActionCmd extends AbstractCmd{
-	public static function run(&$config,$options,$what){
-		$what=self::requiredParam($what, 'controller.action');
-		$scaffold=new ConsoleScaffoldController();
+class NewActionCmd extends AbstractCmd {
+
+	public static function run(&$config, $options, $what) {
+		$what = self::requiredParam($what, 'controller.action');
+		$scaffold = new ConsoleScaffoldController();
 		$scaffold->setConfig($config);
-		@list($controller,$action)=explode('.', $what);
-		if($controller!=null && $action!=null){
-			$controller=self::getCompleteClassname($config, $controller,'controllers');
-			if(class_exists($controller)){
-				$parameters=self::getOption($options, 'p', 'params');
-				$routePath=self::getOption($options, 'r', 'route');
-				$createView=self::getOption($options, 'v', 'create-view',false);
-				$theme=self::getOption($options, 't', 'theme');
-				$routeInfo=null;
-				if($routePath!=null){
-					$routeInfo=["path"=>$routePath,"methods"=>null];
+		@list ($controller, $action) = explode('.', $what);
+		if ($controller != null && $action != null) {
+			$controller = self::getCompleteClassname($config, $controller, 'controllers');
+			if (class_exists($controller)) {
+				$parameters = self::getOption($options, 'p', 'params');
+				$routePath = self::getOption($options, 'r', 'route');
+				$createView = self::getOption($options, 'v', 'create-view', false);
+				$theme = self::getOption($options, 't', 'theme');
+				$routeInfo = null;
+				if ($routePath != null) {
+					$routeInfo = [
+						"path" => $routePath,
+						"methods" => null
+					];
 				}
-				$scaffold->_newAction($controller, $action,$parameters,'',$routeInfo,$createView,$theme);
+				CacheManager::start($config);
+				$scaffold->_newAction($controller, $action, $parameters, '', $routeInfo, $createView, $theme);
+			} else {
+				echo ConsoleFormatter::showMessage("The controller class <b>{$controller}</b> does not exists!", 'error', 'new-action');
 			}
-			else{
-				echo ConsoleFormatter::showMessage("The controller class <b>{$controller}</b> does not exists!",'error','new-action');
-			}
-		}else{
-			echo ConsoleFormatter::showMessage("You must use <b>controller.action</b> notation!",'error','new-action');
+		} else {
+			echo ConsoleFormatter::showMessage("You must use <b>controller.action</b> notation!", 'error', 'new-action');
 		}
 	}
 }
