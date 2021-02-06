42c9b4131161916c4ddb5aeaf2a23fd94945d126
jcheron <myaddressmail@gmail.com>
2021-02-06 01:29:14 +0100

    livereloadx->livereload
    livreloadx does not see file op (add, remove)

--- src/devtools/cmd/commands/LiveReloadCmd.php
+++ src/devtools/cmd/commands/LiveReloadCmd.php
@@ -9,20 +9,21 @@ class LiveReloadCmd extends AbstractCmd {
 
 	public static function run(&$config, $options, $what, $activeDir) {
 		$what ??= $activeDir;
+
 		$port = self::getOption($options, 'p', 'port', 35729);
-		$include = self::getOption($options, 'i', 'include', '*.php');
-		$exclude = self::getOption($options, 'e', 'exclude', '{cache/*,logs/*}');
+		$exts = self::getOption($options, 'e', 'exts', 'php,html');
+		$exclude = self::getOption($options, 'x', 'exclusions', 'cache/,logs/');
 
-		$cmd = "livereloadx {$what} --exclude {$exclude} --include {$include} --port {$port}";
+		$cmd = "livereload {$what} --exclusions {$exclude} --exts {$exts} --port {$port}";
 
-		$msg = "Starting live-reloadx server...\n";
+		$msg = "Starting live-reload server...\n";
 
-		\exec('livereloadx --version', $foo, $exitCode);
+		\exec('livereload --version', $foo, $exitCode);
 
 		if ($exitCode !== 0) {
-			echo ConsoleFormatter::showMessage("Problem starting livereloadx, check livereloadx installation with <b>npm install -g livereloadx</b>.\n", 'warning', 'live-reload');
-			echo ConsoleFormatter::showInfo("Trying to install livereloadx\n");
-			system('npm install -g livereloadx');
+			echo ConsoleFormatter::showMessage("Problem starting livereload, check livereload installation with <b>npm install -g livereloadx</b>.\n", 'warning', 'live-reload');
+			echo ConsoleFormatter::showInfo("Trying to install livereload\n");
+			system('npm install -g livereload');
 		}
 		echo ConsoleFormatter::showInfo($msg . "Press Ctrl+C to stop it!\n");
 		system($cmd . ' &');
