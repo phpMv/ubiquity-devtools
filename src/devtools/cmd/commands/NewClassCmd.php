e3783e18e3f709b76541d725903c71bcc6385e85
jcheron <myaddressmail@gmail.com>
2021-02-06 13:02:33 +0100

    add newClass command

---
+++ src/devtools/cmd/commands/NewClassCmd.php
@@ -0,0 +1,27 @@
+<?php
+namespace Ubiquity\devtools\cmd\commands;
+
+use Ubiquity\devtools\core\ConsoleScaffoldController;
+use Ubiquity\devtools\cmd\ConsoleFormatter;
+use Ubiquity\cache\ClassUtils;
+
+class NewClassCmd extends AbstractCmd {
+
+	public static function run(&$config, $options, $what) {
+		$what = self::requiredParam($what, 'completeClassname');
+		$what = \str_replace('.', "\\", $what);
+		$parent = self::getOption($options, 'p', 'parent', '');
+		$parent = \str_replace('.', "\\", $parent);
+		$classname = ClassUtils::getClassSimpleName($what);
+		$ns = ClassUtils::getNamespaceFromCompleteClassname($what);
+		if ($ns != null) {
+			$scaffold = new ConsoleScaffoldController();
+			$scaffold->setConfig($config);
+			$msg = $scaffold->_createClass('class.tpl', $classname, $ns, '', $parent, '');
+			echo ConsoleFormatter::showMessage("Class <b>{$classname}</b> created!", 'success', 'new-class');
+		} else {
+			echo ConsoleFormatter::showMessage("Class namespace must not be empty!", 'error', 'new-class');
+		}
+	}
+}
+
