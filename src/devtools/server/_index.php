a50769601e3a307868cb45af96e1801bf0e30f64
jcheron <myaddressmail@gmail.com>
2021-04-22 01:32:23 +0200

    Update _index.php

--- src/devtools/server/_index.php
+++ src/devtools/server/_index.php
@@ -1,5 +1,4 @@
 <?php
-error_reporting(E_ALL);
 if (! defined('DS')) {
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT', __DIR__ . \DS . '..' . \DS . 'app' . \DS);
@@ -10,16 +9,23 @@ $config["siteUrl"] = 'http://' . $sConfig['host'] . ':' . $sConfig['port'] . '/'
 $config['sessionName'] = $sConfig['sessionName'];
 require ROOT . './../vendor/autoload.php';
 $config['debug'] = true;
-if (class_exists("\\Monolog\\Logger")) {
+if (\class_exists("\\Monolog\\Logger")) {
 	$config['logger'] = function () use ($sConfig) {
 		return new \Ubiquity\log\libraries\UMonolog($sConfig['sessionName'], \Monolog\Logger::INFO);
 	};
 	\Ubiquity\log\Logger::init($config);
 }
 
+\Ubiquity\debug\Debugger::start();
+
 require ROOT . 'config/services.php';
 
-if (\Ubiquity\debug\Debug::hasLiveReload()) {
-	echo \Ubiquity\debug\Debug::liveReload();
+if (\Ubiquity\debug\LiveReload::hasLiveReload()) {
+	echo \Ubiquity\debug\LiveReload::start();
 }
-\Ubiquity\controllers\Startup::run($config);
+
+try {
+	\Ubiquity\controllers\Startup::run($config);
+} catch (\Throwable $e) {
+	\Ubiquity\debug\Debugger::showException($e);
+}
