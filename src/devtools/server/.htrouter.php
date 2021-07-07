20492ab3950992589308df33c897dbaa8ff50b31
jcheron <myaddressmail@gmail.com>
2021-07-08 01:58:42 +0200

    fix .htrouter pb

--- src/devtools/server/.htrouter.php
+++ src/devtools/server/.htrouter.php
@@ -1,6 +1,15 @@
 <?php
 $uri = \ltrim(\urldecode(\parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)), '/');
-if ($uri !== 'favicon.ico' && ($uri == null || ! \file_exists(__DIR__ . '/../public/' . $uri))) {
+
+if (\substr($uri, 0, 6) === 'vendor' && \file_exists(__DIR__ . '/../' . $uri)) {
+    $fileExts = [
+        'js' => 'application/x-javascript',
+        'css' => 'text/css'
+    ];
+    $pathInfo = \pathinfo($uri);
+    \header("Content-Type: " . $fileExts[$pathInfo['extension']]);
+    \readfile(__DIR__ . '/../' . $uri);
+} elseif ($uri !== 'favicon.ico' && ($uri == null || ! \file_exists(__DIR__ . '/../public/' . $uri))) {
     $_GET['c'] = $uri;
     include_once '_index.php';
     return true;
