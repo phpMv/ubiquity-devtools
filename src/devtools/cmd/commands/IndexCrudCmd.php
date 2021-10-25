ce6b63e874b76fe7699cae9a6dea54e43433d24c
jcheron <myaddressmail@gmail.com>
2021-10-25 20:04:34 +0200

    Update IndexCrudCmd.php

--- src/devtools/cmd/commands/IndexCrudCmd.php
+++ src/devtools/cmd/commands/IndexCrudCmd.php
@@ -4,6 +4,8 @@ namespace Ubiquity\devtools\cmd\commands;
 use Ubiquity\devtools\core\ConsoleScaffoldController;
 use Ubiquity\devtools\cmd\ConsoleFormatter;
 use Ubiquity\cache\CacheManager;
+use Ubiquity\devtools\cmd\Console;
+use Ubiquity\domains\DDDManager;
 
 class IndexCrudCmd extends AbstractCmdModel {
 
@@ -15,7 +17,19 @@ class IndexCrudCmd extends AbstractCmdModel {
 		$crudEvents = self::getOption($options, 'e', 'events', true);
 		$crudViews = self::getOption($options, 't', 'templates', 'index,form,display,home,itemHome');
 		$routePath = self::getOption($options, 'p', 'path', '{resource}');
-		if (strpos($routePath, '{resource') === false) {
+		$domain = self::getOption($options, 'o', 'domain', '');
+		$dbOffset = self::getOption($options, 'a', 'database', '');
+		DDDManager::setDomain($domain);
+		$dbs = DDDManager::getDatabases();
+		if ($dbOffset == '' || \array_search($dbOffset, $dbs) === false) {
+			if (\count($dbs) > 1) {
+				$dbOffset = Console::question('Please select a valid database offset', $dbs);
+			} else {
+				$dbOffset = \current($dbs);
+			}
+			$scaffold->setActiveDb($dbOffset);
+		}
+		if (\strpos($routePath, '{resource') === false) {
 			echo ConsoleFormatter::showMessage("The path variable <b>{$routePath}</b> does not contain the {resource} part!", 'error', 'index-crud-controller');
 		} else {
 			$scaffold->addIndexCrudController($what, $crudDatas, $crudViewer, $crudEvents, $crudViews, $routePath);
