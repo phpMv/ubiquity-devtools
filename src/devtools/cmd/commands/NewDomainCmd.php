e3ea9f30a5128dd7439eb4a99bf046d3906da7f4
jcheron <myaddressmail@gmail.com>
2021-11-01 15:30:39 +0100

    Update NewDomainCmd.php

--- src/devtools/cmd/commands/NewDomainCmd.php
+++ src/devtools/cmd/commands/NewDomainCmd.php
@@ -9,24 +9,29 @@ use Ubiquity\utils\base\UString;
 class NewDomainCmd extends AbstractCmd {
 
 	public static function run(&$config, $options, $what) {
-		$base = self::getOption($options, 'b', 'base');
-		$base = UString::cleanAttribute($base, '_');
 		DDDManager::start();
 		$originalBase = DDDManager::getBase();
+		$base = self::getOption($options, 'b', 'base', $originalBase);
+		$base = UString::cleanAttribute($base, '_');
 		if (DDDManager::hasDomains() && $base !== $originalBase) {
-			$rep = Console::question("The actual base for domains is <b>$originalBase</b>, which is different from <b>$base</b>.\nWould you like to rename the base to <b>$base</b>?", [
+			$rep = Console::question(ConsoleFormatter::formatHtml("The actual base for domains is <b>$originalBase</b>, which is different from <b>$base</b>.\nWould you like to rename the base to <b>$base</b>?"), [
 				'y',
 				'n'
 			]);
 			if (Console::isYes($rep)) {
-				DDDManager::setBase($base);
+				if (! DDDManager::setBase($base)) {
+					echo ConsoleFormatter::showMessage("There was a problem during the base renaming to <b>{$base}</b>!", 'error', 'new-domain');
+				} else {
+					echo ConsoleFormatter::showMessage("Domains base was renamed to <b>{$base}</b>!", 'success', 'new-domain');
+				}
 			}
 		}
 
 		$domain = UString::cleanAttribute($what, '_');
 		if (! DDDManager::domainExists($domain)) {
 			if (DDDManager::createDomain($domain)) {
-				echo ConsoleFormatter::showMessage("The domain <b>{$domain}</b> was successfully created!", 'success', 'new-domain');
+				$domainBase = DDDManager::getDomainBase($domain);
+				echo ConsoleFormatter::showMessage("The domain <b>{$domain}</b> was successfully created in <b>$domainBase</b>!", 'success', 'new-domain');
 			} else {
 				echo ConsoleFormatter::showMessage("There was a problem during the creation of the domain <b>{$domain}</b>!", 'error', 'new-domain');
 			}
