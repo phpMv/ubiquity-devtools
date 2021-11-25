0e3a0e1971db2a9e97729b570cc73fcdffa9573c
jcheron <myaddressmail@gmail.com>
2021-11-25 01:23:19 +0100

    Update MigrationsCmd.php

--- src/devtools/cmd/commands/MigrationsCmd.php
+++ src/devtools/cmd/commands/MigrationsCmd.php
@@ -5,10 +5,12 @@ use Ubiquity\cache\CacheManager;
 use Ubiquity\devtools\cmd\Console;
 use Ubiquity\devtools\cmd\ConsoleFormatter;
 use Ubiquity\devtools\utils\arrays\ClassicArray;
+use Ubiquity\devtools\utils\arrays\ReflectArray;
 use Ubiquity\orm\DAO;
 use Ubiquity\orm\reverse\DatabaseReversor;
 use Ubiquity\db\reverse\DbGenerator;
 use Ubiquity\devtools\cmd\ConsoleTable;
+use Ubiquity\devtools\cmd\Screen;
 
 class MigrationsCmd extends AbstractCmd {
 
@@ -31,34 +33,30 @@ class MigrationsCmd extends AbstractCmd {
 			echo ConsoleFormatter::showMessage("Migrations to operate for db at offset <b>$dbOffset</b>$domainStr:", 'info', 'Migrations');
 			do {
 				self::displayScript(self::scriptToLineArray($script));
-				$rep = Console::question('Select your choices:', [
-					'Execute all commands',
-					'Delete a row',
-					'Quit'
-				]);
-				switch ($rep) {
+				$rep = Console::question('Select your choices:', ['Execute all commands', 'Delete a row', 'Quit']);
+				switch ($rep){
 					case 'Delete a row':
-						$count = \count($script);
-						$row = Console::question("Enter a valid row between 1 and $count:");
-						$delete = self::deleteScriptRow($row, $script);
-						if ($delete !== false) {
-							$script = $delete;
+						$count=\count($script);
+						$row=Console::question("Enter a valid row between 1 and $count:");
+						$delete=self::deleteScriptRow($row,$script);
+						if($delete!==false){
+							$script=$delete;
 						}
 						break;
 					case 'Execute all commands':
-						self::executeSQLTransaction($dbOffset, implode(';', $script), 'Database migrations');
-						$rep = 'Quit';
+						self::executeSQLTransaction($dbOffset,implode(';',$script),'Database migrations');
+						$rep='Quit';
 						break;
 					default:
 						echo ConsoleFormatter::showInfo('Operation terminated, Bye!');
 				}
-			} while ($rep !== 'Quit');
+			}while($rep!=='Quit');
 		}
 	}
-
-	private static function deleteScriptRow(int $rowNum, array $script) {
-		$rowNum --;
-		if ($rowNum >= 0 && $rowNum < \count($script)) {
+	
+	private static function deleteScriptRow(int $rowNum,array $script) {
+		$rowNum--;
+		if($rowNum>=0 && $rowNum<\count($script)){
 			unset($script[$rowNum]);
 			return array_values($script);
 		}
@@ -76,26 +74,26 @@ class MigrationsCmd extends AbstractCmd {
 	private static function scriptToLineArray(array $script): array {
 		$result = [];
 		$line = 1;
+		$width=Screen::getWidth()-20;
 		foreach ($script as $sql) {
-			$result[] = [
-				'line' => $line ++,
-				'sql' => $sql
+			$result []= [
+				 'line'=>$line ++,'sql'=> \wordwrap($sql, $width,PHP_EOL)
 			];
 		}
 		return $result;
 	}
 
-	private static function executeSQLTransaction(string $activeDbOffset, string $sql, string $title) {
+	private static function executeSQLTransaction(string $activeDbOffset,string $sql, string $title) {
 		$isValid = true;
 		if (isset($sql)) {
-			$db = DAO::getDatabase($activeDbOffset ?? 'default');
+			$db = DAO::getDatabase($activeDbOffset??'default');
 			if (! $db->isConnected()) {
 				$db->setDbName('');
 				try {
 					$db->connect();
 				} catch (\Exception $e) {
 					$isValid = false;
-					echo ConsoleFormatter::showMessage($e->getMessage(), 'error', $title);
+					echo ConsoleFormatter::showMessage($e->getMessage(), 'error',$title);
 				}
 			}
 			if ($isValid) {
@@ -105,7 +103,7 @@ class MigrationsCmd extends AbstractCmd {
 						if ($db->inTransaction()) {
 							$db->commit();
 						}
-						echo ConsoleFormatter::showMessage("Database created/updated with success at offset $activeDbOffset!", 'success', $title);
+						echo ConsoleFormatter::showMessage( "Database created/updated with success at offset $activeDbOffset!", 'success', $title);
 					} catch (\Error $e) {
 						if ($db->inTransaction()) {
 							$db->rollBack();
@@ -114,7 +112,7 @@ class MigrationsCmd extends AbstractCmd {
 					}
 				} else {
 					$db->execute($sql);
-					echo ConsoleFormatter::showMessage("Database created/updated with success at offset $activeDbOffset!", 'success', $title);
+					echo ConsoleFormatter::showMessage( "Database created/updated with success at offset $activeDbOffset!", 'success',$title);
 				}
 			}
 		}
