637b8c96d02ca7949c9001afec7c688ae519403d
jcheron <myaddressmail@gmail.com>
2022-10-25 02:49:58 +0200

    Update index.php

--- src/devtools/project-files/index.php
+++ src/devtools/project-files/index.php
@@ -1,7 +1,7 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__ . DS . '..' . DS . 'app' . DS);
-$config = include ROOT . 'config/config.php';
+$config = include ROOT . 'cache/config/config.cache.php';
 require ROOT . './../vendor/autoload.php';
 require ROOT . 'config/services.php';
 \Ubiquity\controllers\Startup::run($config);
