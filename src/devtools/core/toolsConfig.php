17634d9a8e287cbde7bb23d0ccdc49527affb25f
J-C HERON <myaddressmail@gmail.com>
2025-11-30 19:48:41 +0100

    [skip ci] Update Semantic UI and jQuery versions, restyle theme
    Upgraded Fomantic UI to 2.9.4 and jQuery to 3.7.x in configuration and template files. Updated PHP minimum version requirement to 8.1. Overhauled semantic theme CSS for a modern look, including new color variables, gradients, layout, and improved header/footer styling.

--- src/devtools/core/toolsConfig.php
+++ src/devtools/core/toolsConfig.php
@@ -7,8 +7,8 @@ return [
 			"js" => "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"
 		],
 		"semantic" => [
-			"css" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.3/dist/semantic.min.css",
-			"js" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.3/dist/semantic.min.js"
+			"css" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.4/dist/semantic.min.css",
+			"js" => "https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.4/dist/semantic.min.js"
 		],
 		"diff2html" => [
 			"css" => "https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.12.2/diff2html.min.css"
@@ -16,7 +16,7 @@ return [
 	],
 	"composer" => [
 		"require" => [
-			"php" => ">=7.4",
+			"php" => ">=8.1",
 			"twig/twig" => "^3.0",
 			"phpmv/ubiquity" => "^2.5"
 		],
